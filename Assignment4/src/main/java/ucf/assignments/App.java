469cb43eb68c5ee36467e4aa36b276cad68fe1d6
powakick <90665243+powakick@users.noreply.github.com>
2021-11-01 22:39:23 -0400

    Add files via upload

---
+++ Assignment4/src/main/java/ucf/assignments/App.java
@@ -0,0 +1,80 @@
+package ucf.assignments;
+
+/* UCF COP330 Fall 2021 Assignment 4 Solution
+copyright 2021 Jonathan Tsui
+ */
+
+
+
+
+
+
+public class App {
+public static void main(String[] args){
+
+}
+
+    static void addTodoList(){
+/* psuedo code:
+
+ */
+    }
+
+    static void removeTodoList(){
+
+    }
+
+    static void editTodolistName() {
+
+    }
+
+    static void additem(){
+
+    }
+
+    static void removeitem(){
+
+    }
+
+    static void editdescription(){
+
+    }
+
+    static void editdate(){
+
+    }
+
+    static void complete(){
+
+    }
+
+    static void displayexistingitems(){
+
+    }
+
+    static void displayincompleted(){
+
+    }
+
+    static void displaycompleted(){
+
+    }
+
+    static void savesingletodolist(){
+
+    }
+
+    static void savealltodolists(){
+
+    }
+
+    static void opentodolist(){
+
+    }
+
+    static void openmultipletodolists(){
+
+    }
+
+
+}
